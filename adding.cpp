4721c1d1e053d6442af790b9ac778c7c2d9226de
Shant <113237039+Shant2324@users.noreply.github.com>
2022-09-18 16:46:14 +0530

    Create adding.cpp

---
+++ adding.cpp
@@ -0,0 +1,14 @@
+//C++ code of adding two numbers, same format can be used to subtracting, multiplication or dividing two numbers
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  int a,b,c;
+  cout<<"Enter first number: ";
+  cin>>a;
+  cout<<"Enter second number: ";
+  cin>>b;
+  c=a+b;
+  cout<<"Addition of both numbers is: "<<c;
+  getch();
+}
