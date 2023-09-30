cba815e250a0094be3c3bc4f75c19ce594ccf6c1
Shant <113237039+Shant2324@users.noreply.github.com>
2023-09-30 17:36:22 +0530

    Create Greatest.cpp

---
+++ Greatest.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  int a,b;
+  clrscr();   
+  cout<<"Enter first number: ";
+  cin>>a;
+  cout<<"Enter second number: ";
+  cin>>b;
+  if a>b:
+    cout<<"The greater number is: "<<a;
+  else:
+    cout<<"The greater number is: "<<b;
+  getch();
+}
