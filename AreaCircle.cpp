34e678b73e23e0c461e6dc85e788d9f2c469e359
Shant <113237039+Shant2324@users.noreply.github.com>
2023-09-30 11:56:30 +0530

    Create AreaCircle.cpp

---
+++ AreaCircle.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  int r;
+  float pi, A;
+  clrscr();  
+  pi=3.14
+  cout<<"Enter the radius of the circle: ";
+  cin>>r;
+  A=2*pi*r;
+  cout<<"Area of the circle is: "<<A;
+  getch();
+}
