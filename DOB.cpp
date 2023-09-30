66cc0ce8e84a31cba1ddc6b973bdf37d0cd7fc0e
Shant <113237039+Shant2324@users.noreply.github.com>
2023-09-30 12:05:32 +0530

    Create DOB.cpp

---
+++ DOB.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  int CrntYr,BrtYr,Age;
+  clrscr();   
+  cout<<"Enter the current Year: ";
+  cin>>CrntYr;
+  cout<<"Enter your age: ";
+  cin>>Agr;
+  BrtYr=Crntyr-Age;
+  cout<<"You was born in Year: "<<Age;
+  getch();
+}
